6caee44a943578f305fb67ebb8ff796feb831e27
Yolaus <yolausgodoy07@gmail.com>
2026-03-23 13:29:26 -0300

    Antes de insertar Pokemons por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,25 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
+    HttpModule,
+    
     PokemonModule,
 
     CommonModule,
+
+    SeedModule,
   ],
 })
 export class AppModule { }
